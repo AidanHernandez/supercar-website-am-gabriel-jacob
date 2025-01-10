16df8d85269a139fe488407519321bd67708d49f
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-01-09 21:58:06 -0500

    about us done

--- js/about-us.js
+++ js/about-us.js
@@ -1,13 +1,65 @@
 var next = document.getElementById("next")
+var back = document.getElementById("back")
 
-var page
+var page = 0
 
 next.addEventListener("click", change)
+back.addEventListener("click", change_back)
 
 function change(){
-    page = page + 1
-    if(page == 2){
-        document.querySelector('#myImage').src = 'newImagePath.jpg';
+    
+    if(page < 2)
+    {
+        page = page + 1
+        console.log(page)
+        if(page == 1){
+            document.getElementById('img1').src = 'img/about-us-img/quinton.png'
+            document.getElementById('text1').innerText = "Quinton Dodson\nVP of Special Vehicle Operations"
+            back.style.fill = 'white'
 
+            document.getElementById('img2').src = 'img/about-us-img/kai.png'
+            document.getElementById('text2').innerText = "Kie Fair\nVP pf Sales/ Dealer Coordinator"
+
+        }
+        else if(page == 2){
+            document.getElementById('img1').src = 'img/about-us-img/romero.png'
+            document.getElementById('text1').innerText = "Romeo Miller\nChief Brand Ambassador"
+
+            document.getElementById('img2').src = 'img/about-us-img/warren.png'
+            document.getElementById('text2').innerText = "Warren Hollier\nDirector Of Special Programs"
+
+            next.style.fill = '#6E6E6E'
+            
+        }
     }
+}
+
+function change_back(){
+    if(page > 0)
+        {
+            page = page - 1
+            console.log(page)
+            if(page == 1){
+                document.getElementById('img1').src = 'img/about-us-img/quinton.png'
+                document.getElementById('text1').innerText = 'Quinton Dodson\nVP of Special Vehicle Operations'
+                next.style.fill = 'white'
+                
+    
+                
+                document.getElementById('img2').src = 'img/about-us-img/kai.png'
+                document.getElementById('text2').innerText = "Kie Fair\nVP pf Sales/ Dealer Coordinator"
+    
+            }
+            else if(page == 0){
+                
+                document.getElementById('img1').src = 'img/about-us-img/brentjohnson.png'
+                document.getElementById('text1').innerText = "Brent Johnson\nChief Operating Officer"
+    
+                document.getElementById('img2').src = 'img/about-us-img/nancypatterson.png'
+                document.getElementById('text2').innerText = "Nancy Patterson\nChief Executive Assistant"
+    
+                back.style.fill = '#6E6E6E'
+                
+            }
+        }
 }
