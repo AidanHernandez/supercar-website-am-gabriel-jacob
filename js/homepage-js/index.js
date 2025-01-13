1d203fbdc766070f6318e2aa0638695dfd4323c0
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-01-12 19:02:59 -0800

    index working

--- js/homepage-js/index.js
+++ js/homepage-js/index.js
@@ -74,23 +74,76 @@ function carosel_option4(){
 function side_feature_open(){
     if( side_feature_clicked == false)
     {
-        // document.getElementById("scroll-car").style.bottom = "-55.7%"
-        document.getElementById("scroll-car").style.marginLeft = "50%"
-        side_feature_clicked = true
-        sideblocker.style.animationName = "sideblocker-out"
+        if(window.matchMedia("(max-width:2251px)").matches && window.matchMedia("(min-width: 1851px)").matches){
+            document.getElementById("scroll-car").style.marginLeft = "1000px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+        else if(window.matchMedia("(max-width:1851px)").matches && window.matchMedia("(min-width: 1650px)").matches){
+            document.getElementById("scroll-car").style.marginLeft = "600px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+        else if(window.matchMedia("(max-width:1651px)").matches && window.matchMedia("(min-width: 1101px)").matches){
+            document.getElementById("scroll-car").style.marginLeft = "600px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+        else if(window.matchMedia("(max-width:1101px)").matches && window.matchMedia("(min-width: 901px)").matches){
+            document.getElementById("scroll-car").style.marginLeft = "450px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+        else if(window.matchMedia("(max-width:901px)").matches && window.matchMedia("(min-width: 769px)").matches){
+            document.getElementById("scroll-car").style.marginLeft = "350px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+        else if(window.matchMedia("(max-width:769px)").matches && window.matchMedia("(min-width: 571px)").matches){
+            document.getElementById("scroll-car").style.marginLeft = "250px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+        else if(window.matchMedia("(max-width:571px)").matches && window.matchMedia("(min-width: 571px)").matches){
+            document.getElementById("scroll-car").style.marginLeft = "190px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+        else if(window.matchMedia("(max-width:426px)").matches ){
+            document.getElementById("scroll-car").style.marginLeft = "150px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+        else{
+            document.getElementById("scroll-car").style.marginLeft = "1300px"
+            side_feature_clicked = true
+            sideblocker.style.animationName = "sideblocker-out"
+        }
+       
+
 
         
         
     }
     else{
-        
-        document.getElementById("scroll-car").style.marginLeft = "5%"
-        // document.getElementById("side-scroll-feature").style.width = "37.5%"
-
+    if(window.matchMedia("(max-width:901px)").matches && window.matchMedia("(min-width: 426px)").matches){
+        document.getElementById("scroll-car").style.marginLeft = "50px"
+            sideblocker.style.animationName = "sideblocker-in"
+            side_feature_clicked = false
+    }
+    else if(window.matchMedia("(max-width:426px)").matches ){
+        document.getElementById("scroll-car").style.marginLeft = "10px"
         sideblocker.style.animationName = "sideblocker-in"
-
-
         side_feature_clicked = false
+    }
+    else{
+        document.getElementById("scroll-car").style.marginLeft = "200px"
+            sideblocker.style.animationName = "sideblocker-in"
+            side_feature_clicked = false
+    }
+            
+        
+           
     }
 }
 
